aabae0e49f33fb0e490f8ae4a313b7d40f3e231c
Yunic10 <yudithnico99@gmail.com>
2023-11-24 00:04:55 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appmhs',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
